90da837ba395c824ed800c4b2a2053071b633a88
NilsBade <nils.bade@egnition-hamburg.de>
2024-04-25 12:13:27 +0200

    configured venv for pytest (run "python -m pytest"). set fixtures to Unittest chamber connect. startet jog commands

--- PythonChamberApp/chamber_net_interface/chamber_net_interface.py
+++ PythonChamberApp/chamber_net_interface/chamber_net_interface.py
@@ -5,7 +5,7 @@ and implements all high level commands necessary to control the measurement proc
 Server response status codes:
 200 -   Ok
 204 -   No Content (no error)
-400 -   Server did not understand message
+400 -   Bad Request (server did not understand message)
 403 -   Server denied access
 409 -   Conflict
 """
@@ -16,8 +16,64 @@ import requests
 
 
 class ChamberNetworkCommands(connection_handler.NetworkDevice):
-    def __init__(self, ip_address:str = None, api_key:str = None):
+
+    # private properties
+    header_api = None
+    header_tjson = None
+
+    def __init__(self, ip_address: str = None, api_key: str = None):
+        """stores ip address of chamber and initializes private standard headers"""
         super().set_ip_address('http://' + ip_address)
         super().set_api_key(api_key)
+        self.header_api = {
+            'X-Api-Key': super().get_api_key()
+        }
+        self.header_tjson = {
+            'X-Api-Key': super().get_api_key(),
+            'Content-Type': 'application/json'
+        }
+
+    def chamber_connect(self):
+        """Initiates Octoprint serial connection to chamber (printer).
+        Returns dict of ['status_code' : str , 'content' : str] from server response"""
+        url = super().get_ip_address() + '/api/connection'
+        payload = {
+            "command": "connect"
+        }
+        response = requests.post(url=url, headers=self.header_tjson, json=payload)
+        return {
+            'status_code': response.status_code,
+            'content': response.content
+        }
+
+    def chamber_disconnect(self):
+        """Requests Octoprint to disconnect serial port to chamber (printer).
+        Returns dict of ['status_code' : str , 'content' : str] from server response"""
+        url = super().get_ip_address() + '/api/connection'
+        payload = {
+            "command": "disconnect"
+        }
+        response = requests.post(url=url, headers=self.header_tjson, json=payload)
+        return {
+            'status_code': response.status_code,
+            'content': response.content
+        }
+
+    def chamber_jog_rel(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
+        url = super().get_ip_address() + 'api/printer/printhead'
+        payload = {
+            "command": "jog",
+            "x": x,
+            "y": y,
+            "z": z,
+            "absolute": False
+        }
+        response = requests.post(url, headers=self.header_tjson, json=payload)
+        return {
+            'status_code': response.status_code,
+            'content': response.content
+        }
+
+    # Kümmer dich im den Jog-Speed!
 
 
--- tests/unit/test_connection_handler.py
+++ tests/unit/test_connection_handler.py
@@ -3,9 +3,8 @@
 import PythonChamberApp.connection_handler as connection_handler
 
 def test_network_device_url():
+    """Checks if ip-address / url can be set and gitHub webpage is reachable"""
     new_dev = connection_handler.NetworkDevice()
     new_dev.set_ip_address("https://github.com/NilsBade/PythonChamberApp")
     response = new_dev.check_if_reachable()
     return
-
-
