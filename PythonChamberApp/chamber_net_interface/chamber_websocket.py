7a77c7dd2c6c8e908e0353b6e039bc7eb3b5a132
NilsBade <nils.bade@egnition-hamburg.de>
2024-04-26 10:15:37 +0200

    Stopped development of websocket connection. Cleaned up project to not include websocket class and tests. - return to cmd-class-development

--- PythonChamberApp/chamber_net_interface/chamber_websocket.py
+++ PythonChamberApp/chamber_net_interface/chamber_websocket.py
@@ -1,9 +1,20 @@
+"""
+Can not manage to open websocket and authenticate successfully...
+Don't quite get how to do authentication via websocket module and what octoprint expects.
+--> This topic is cancelled unless viable reasons occur why the information "what klipper **thinks**
+    where the print-head is", is an important information. For now the chamber will be assumed to follow all
+    instructions (exactly like in GCode) without further checking for errors.
+"""
+
 
 import requests
-import json
 import websocket
+import json
 
+
+## Websocket tests
 # chamber_ip = 'http://134.28.25.201'
+
 
 class Chamber_websocket():
 
@@ -30,10 +41,48 @@ class Chamber_websocket():
         return
 
     def get_properties(self):
+        """returns dict of {'ip_address': str, 'auth_key': str}"""
         print('\nip_address: ' + self.ip_address + ' | auth-key: ' + self.auth_socket_key)
-        return {'ip_address': self.ip_address, 'auth-key': self.auth_socket_key}
+        return {'ip_address': self.ip_address, 'auth_key': self.auth_socket_key}
 
 
+
+def on_message(ws, message):
+    print("Received:", message)
+
+
+def on_error(ws, error):
+    print("Error:", error)
+
+
+def on_close(ws):
+    print("### Connection closed ###")
+
+
+def on_open(ws):
+    print("### Connection opened ###")
+
+
+if __name__ == "__main__":
+    new_ws = Chamber_websocket('134.28.25.201')
+    properties = new_ws.get_properties()
+    # Replace 'ws://example.com/sockjs_endpoint' with the actual SockJS endpoint URL
+    websocket.enableTrace(True)  # Enable trace to see connection details
+    ws = websocket.WebSocketApp(url='ws://134.28.25.201/sockjs/websocket',
+                                on_message=on_message,
+                                on_error=on_error,
+                                on_close=on_close
+                                )
+    ws.on_open = on_open
+    ws.run_forever()
+
+
+    authenticate = '{\"auth\": \"bade:' + properties['auth_key'] + '"}'
+    print(authenticate)
+    ws.send(authenticate)
+    data = '{"subscribe": {"events": ["PositionUpdate"]}}'
+    print(data)
+    ws.send(data=data)
 
 
 
--- tests/integration/test_chamber_websocket.py
+++ tests/integration/test_chamber_websocket.py
@@ -1,11 +1,13 @@
 """
 These tests can only be run when connected to the local network of the
 measurement chamber and the chamber_ip_address parameter is correct!
+
+--> these tests can only be run once the chamber_net_interface.__init__ again imports Chamber_websocket Class
 """
 import pytest
 import PythonChamberApp.chamber_net_interface as chamber_cmd
 
-
+"""
 @pytest.fixture
 def chamber_ip_address():
     return '134.28.25.201'
@@ -16,3 +18,7 @@ def test_chamber_websocket(chamber_ip_address):
     properties = new_socket.get_properties()
     assert properties['ip_address'] == 'http://134.28.25.201', f"hallo"
     return
+
+...
+
+"""
