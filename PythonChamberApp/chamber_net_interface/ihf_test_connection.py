c30626944a5c0200e224850383780f12e8bd5e23
NilsBade <nils.bade@egnition-hamburg.de>
2024-04-26 10:15:37 +0200

    Stopped development of websocket connection. Cleaned up project to not include websocket class and tests. - return to cmd-class-development

--- PythonChamberApp/chamber_net_interface/ihf_test_connection.py
+++ PythonChamberApp/chamber_net_interface/ihf_test_connection.py
@@ -44,32 +44,3 @@ print(response.status_code)
 url4 = chamber_ip + '/api/printer'
 response = requests.get(url4, headers=headers_api)
 
-## Websocket tests
-import websocket
-
-
-def on_message(ws, message):
-    print("Received:", message)
-
-
-def on_error(ws, error):
-    print("Error:", error)
-
-
-def on_close(ws):
-    print("### Connection closed ###")
-
-
-def on_open(ws):
-    print("### Connection opened ###")
-
-
-if __name__ == "__main__":
-    # Replace 'ws://example.com/sockjs_endpoint' with the actual SockJS endpoint URL
-    websocket.enableTrace(True)  # Enable trace to see connection details
-    ws = websocket.WebSocket()
-    ws.connect('ws://134.28.25.201/sockjs/websocket',
-               on_message=on_message,
-               on_error=on_error,
-               on_close=on_close)
-    print(ws.recv())
