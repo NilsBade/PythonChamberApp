49dad727caf32fccc0dddfec08559e697c937262
NilsBade <nils.bade@egnition-hamburg.de>
2024-04-30 14:47:15 +0200

    Blueprint for AutoMeasurement class as seperate thread ready for test with sample coordinate vectors

--- PythonChamberApp/process_controller/multithread_worker.py
+++ PythonChamberApp/process_controller/multithread_worker.py
@@ -1,6 +1,6 @@
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
+from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot, QRunnable
 
 import time
 import traceback, sys
@@ -22,13 +22,13 @@ class WorkerSignals(QObject):
         object data returned from processing, anything
 
     progress
-        int indicating % progress
+        dict with key-value pairs dependend on usecase
 
     '''
     finished = pyqtSignal()
     error = pyqtSignal(tuple)
     result = pyqtSignal(object)
-    progress = pyqtSignal(int)
+    progress = pyqtSignal(dict)
 
 
 class Worker(QRunnable):
@@ -76,72 +76,3 @@ class Worker(QRunnable):
             self.signals.finished.emit()  # Done
 
 
-
-class MainWindow(QMainWindow):
-
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-
-        self.counter = 0
-
-        layout = QVBoxLayout()
-
-        self.l = QLabel("Start")
-        b = QPushButton("DANGER!")
-        b.pressed.connect(self.oh_no)
-
-        layout.addWidget(self.l)
-        layout.addWidget(b)
-
-        w = QWidget()
-        w.setLayout(layout)
-
-        self.setCentralWidget(w)
-
-        self.show()
-
-        self.threadpool = QThreadPool()
-        print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
-
-        self.timer = QTimer()
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.recurring_timer)
-        self.timer.start()
-
-    def progress_fn(self, n: int):
-        print(str(n) + '%')
-
-    def execute_this_fn(self, progress_callback):
-        for n in range(0, 9):
-            time.sleep(0.5)
-            prog = n*100/8
-            progress_callback.emit(int(prog))
-
-        return "Done."
-
-    def print_output(self, s):
-        print(s)
-
-    def thread_complete(self):
-        print("THREAD COMPLETE!")
-
-    def oh_no(self):
-        # Pass the function to execute
-        worker = Worker(self.execute_this_fn) # Any other args, kwargs are passed to the run function
-        worker.signals.result.connect(self.print_output)
-        worker.signals.finished.connect(self.thread_complete)
-        worker.signals.progress.connect(self.progress_fn)
-
-        # Execute
-        self.threadpool.start(worker)
-
-
-    def recurring_timer(self):
-        self.counter +=1
-        self.l.setText("Counter: %d" % self.counter)
-
-
-app = QApplication([])
-window = MainWindow()
-app.exec()
