e06b534241ef1eb31d4da33f1134ed5c654297d9
NilsBade <nils.bade@egnition-hamburg.de>
2024-04-30 10:50:47 +0200

    Fixed empty server responses by adding a 'M105' command to jog requests. Quick-Fixed window updates by static process-Event-function. Upgrade to multithread!

--- PythonChamberApp/user_interface/ui_mainwindow.py
+++ PythonChamberApp/user_interface/ui_mainwindow.py
@@ -3,7 +3,7 @@ This file stores the mainwindow class and all necessary PyQt6 classes to constru
 """
 
 import sys
-from PyQt6.QtWidgets import QMainWindow, QWidget, QStatusBar, QTabWidget
+from PyQt6.QtWidgets import QMainWindow, QWidget, QStatusBar, QTabWidget, QMessageBox
 from PythonChamberApp.user_interface.ui_config_window import UI_config_window
 from PythonChamberApp.user_interface.ui_chamber_control_window import UI_chamber_control_window
 from PythonChamberApp.user_interface.ui_vna_control_window import UI_vna_control_window
@@ -53,3 +53,30 @@ class MainWindow(QMainWindow):
         self.tabs.setTabEnabled(2, False)
 
         self.setCentralWidget(self.tabs)
+        return
+
+    def prompt_info(self, info_msg: str, window_title: str):
+        """
+        Opens a Dialog Window with Info mark in front of the main window.
+        Displays given info_msg and blocks rest of the application until accepted.
+        """
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle(window_title)
+        dlg.setText(info_msg)
+        dlg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        dlg.setIcon(QMessageBox.Icon.Question)
+        dlg.exec()
+        return
+
+    def prompt_warning(self, warn_msg: str, window_title: str):
+        """
+        Opens a Warning Window with Warning mark in front of the main window.
+        Displays given warn_msg and blocks rest of the application until accepted.
+        """
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle(window_title)
+        dlg.setText(warn_msg)
+        dlg.setStandardButtons(QMessageBox.StandardButton.Ok)
+        dlg.setIcon(QMessageBox.Icon.Warning)
+        dlg.exec()
+        return
