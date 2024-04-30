45548ec92c3b7b267a6fb5f7f1f7af3053214e2a
NilsBade <nils.bade@egnition-hamburg.de>
2024-04-30 11:35:15 +0200

    corrected typo in doc-string

--- PythonChamberApp/user_interface/multithread_example.py
+++ PythonChamberApp/user_interface/multithread_example.py
@@ -37,19 +37,19 @@ class Worker(QRunnable):
 
     Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
 
+    :type callback: function
     :param callback: The function callback to run on this worker thread. Supplied args and
                      kwargs will be passed through to the runner.
-    :type callback: function
     :param args: Arguments to pass to the callback function
     :param kwargs: Keywords to pass to the callback function
 
     '''
 
-    def __init__(self, fn, *args, **kwargs):
+    def __init__(self, callback, *args, **kwargs):
         super(Worker, self).__init__()
 
         # Store constructor arguments (re-used for processing)
-        self.fn = fn
+        self.callback = callback
         self.args = args
         self.kwargs = kwargs
         self.signals = WorkerSignals()
