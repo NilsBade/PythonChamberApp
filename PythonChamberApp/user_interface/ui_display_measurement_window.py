e6aa9fe2f19429545fa2cd49ba40656fc059cdc1
nils bade <nils.badee@@egn-hh.de>
2024-06-07 15:07:59 +0200

    started measurement-display tab coding. added a sample measurement to test measurement-display.

--- PythonChamberApp/user_interface/ui_display_measurement_window.py
+++ PythonChamberApp/user_interface/ui_display_measurement_window.py
@@ -1,6 +1,33 @@
-from PyQt6.QtWidgets import QWidget, QLineEdit, QLabel
+from PyQt6.QtWidgets import QWidget, QLineEdit, QLabel, QBoxLayout, QComboBox, QPushButton, QTextEdit
 
 
 class UI_display_measurement_window(QWidget):
+
+    # Properties
+
+    def __init__(self):
+        super().__init__()
+
+        data_selection_widget =
+
+
+
+    def __init_data_selection_widget(self):
+        main_widget = QWidget()
+
+        return main_widget
+
+    def __init_data_details_widget(self):
+        main_widget = QWidget()
+
+        return main_widget
+
+    def __init_data_plot_widget(self):
+        main_widget = QWidget()
+
+        return main_widget
+
+
+
 
 
