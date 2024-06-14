0d199ba5d3a01d85512317e45f1510ec550891db
NilsBade <nils.bade@egnition-hamburg.de>
2024-06-14 11:35:37 +0200

    added timestamps to compare reduced data size storing and large data size (dict structure)

--- tests/Scripts/gen_script_sample_data.py
+++ tests/Scripts/gen_script_sample_data.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+from datetime import datetime
 
 data: dict = {}
 vna_info = {
@@ -39,6 +40,7 @@ data['S11'] = []
 data['S12'] = []
 data['S22'] = []
 
+print(f"Starting first loop for large data sample... {datetime.now().strftime('%H:%M:%S')}")
 for z in np.linspace(data['measurement_config']['mesh_z_min'], data['measurement_config']['mesh_z_max'], data['measurement_config']['mesh_z_steps']):
     for y in np.linspace(data['measurement_config']['mesh_y_min'], data['measurement_config']['mesh_y_max'],data['measurement_config']['mesh_y_steps']):
         for x in np.linspace(data['measurement_config']['mesh_x_min'], data['measurement_config']['mesh_x_max'],data['measurement_config']['mesh_x_steps']):
@@ -46,8 +48,28 @@ for z in np.linspace(data['measurement_config']['mesh_z_min'], data['measurement
                 data['S11'].append([x, y, z, f, x**2*y**2*f, 180])
                 data['S12'].append([x, y, z, f, x*z*f, x*y])
                 data['S22'].append([x, y, z, f, x*y*z, 90])
-
+print(f"Write large data sample to file... {datetime.now().strftime('%H:%M:%S')}")
 file = open('../../results/script_sample_data.json', 'w')
-file.write(json.dumps(data, indent=4))
+file.write(json.dumps(data, indent=2))
+print(f"Writing done {datetime.now().strftime('%H:%M:%S')}")
 file.close()
+
+red_data = {
+    'measurement_config': data['measurement_config'],
+    'data': [],
+}
+print(f"Starting second loop for reduced data sample... {datetime.now().strftime('%H:%M:%S')}")
+for z in np.linspace(data['measurement_config']['mesh_z_min'], data['measurement_config']['mesh_z_max'], data['measurement_config']['mesh_z_steps']):
+    for y in np.linspace(data['measurement_config']['mesh_y_min'], data['measurement_config']['mesh_y_max'],data['measurement_config']['mesh_y_steps']):
+        for x in np.linspace(data['measurement_config']['mesh_x_min'], data['measurement_config']['mesh_x_max'],data['measurement_config']['mesh_x_steps']):
+            for f in np.linspace(vna_info['freq_start'], vna_info['freq_stop'], vna_info['sweep_num_points']):
+                red_data['data'].append([x, y, z, f, x**2*y**2*f, 180, x*z*f, x*y, x*y*z, 90])
+                #red_data_syntax = [ (0)[(0)x, (1)y, (2)z, (3)frequency, (4)S11-amplitude, (5)S11-phase, (6)S12-amplitude, (7)S12-phase, (8)S22-amplitude, (9)S22-phase], (1)[..], .. ]
+
+print(f"Write reduced data sample to file... {datetime.now().strftime('%H:%M:%S')}")
+red_file = open('../../results/reduced_script_sample_data.json', 'w')
+red_file.write(json.dumps(red_data, indent=2))
+print(f"Writing done {datetime.now().strftime('%H:%M:%S')}")
+
+red_file.close()
 
