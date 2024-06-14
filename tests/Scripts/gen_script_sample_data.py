2f126492411c14ba3ac6cd7e676e5fbdcdd4a40f
NilsBade <nils.bade@egnition-hamburg.de>
2024-06-14 09:18:37 +0200

    removed script generated data from git and added to gitignore explicitly to prevent too large file sizes on git

--- tests/Scripts/gen_script_sample_data.py
+++ tests/Scripts/gen_script_sample_data.py
@@ -16,27 +16,15 @@ data['measurement_config'] = {
                 'type':             'Auto Measurement Data JSON',
                 'timestamp':        "Uhrzeit und Datum",
                 'zero_position':    [250.0, 300.0, 0.0],
-<<<<<<< HEAD
-                'mesh_x_min':       150, #[mm]
-                'mesh_x_max':       350, #[mm]
-                'mesh_x_steps':     100,
-                'mesh_y_min':       180, #[mm]
-                'mesh_y_max':       420, #[mm]
-                'mesh_y_steps':     120,
-                'mesh_z_min':       10, #[mm]
-                'mesh_z_max':       110, #[mm]
-                'mesh_z_steps':     11,
-=======
                 'mesh_x_min':       245, #[mm]
                 'mesh_x_max':       255, #[mm]
-                'mesh_x_steps':     11,
+                'mesh_x_steps':     100,
                 'mesh_y_min':       295, #[mm]
                 'mesh_y_max':       305, #[mm]
-                'mesh_y_steps':     11,
+                'mesh_y_steps':     100,
                 'mesh_z_min':       100, #[mm]
                 'mesh_z_max':       110, #[mm]
                 'mesh_z_steps':     20,
->>>>>>> d3fcb55ff57cd8bfa21b0870617ee122d9e692fe
                 'movespeed':        10, #[mm/s]
                 'parameter':        vna_info['parameter'],
                 'freq_start':       vna_info['freq_start'], #[Hz]
@@ -50,18 +38,13 @@ data['measurement_config'] = {
 data['S11'] = []
 data['S12'] = []
 data['S22'] = []
-<<<<<<< HEAD
+
 for z in np.linspace(data['measurement_config']['mesh_z_min'], data['measurement_config']['mesh_z_max'], data['measurement_config']['mesh_z_steps']):
     for y in np.linspace(data['measurement_config']['mesh_y_min'], data['measurement_config']['mesh_y_max'],data['measurement_config']['mesh_y_steps']):
         for x in np.linspace(data['measurement_config']['mesh_x_min'], data['measurement_config']['mesh_x_max'],data['measurement_config']['mesh_x_steps']):
-=======
-for z in np.linspace(100, 110, data['measurement_config']['mesh_z_steps']):
-    for y in np.linspace(-5,5,data['measurement_config']['mesh_y_steps']):
-        for x in np.linspace(-5,5,data['measurement_config']['mesh_x_steps']):
->>>>>>> d3fcb55ff57cd8bfa21b0870617ee122d9e692fe
             for f in np.linspace(vna_info['freq_start'], vna_info['freq_stop'], vna_info['sweep_num_points']):
-                data['S11'].append([x, y, z, f, x**2*y**2, 180])
-                data['S12'].append([x, y, z, f, z, x*y])
+                data['S11'].append([x, y, z, f, x**2*y**2*f, 180])
+                data['S12'].append([x, y, z, f, x*z*f, x*y])
                 data['S22'].append([x, y, z, f, x*y*z, 90])
 
 file = open('../../results/script_sample_data.json', 'w')
