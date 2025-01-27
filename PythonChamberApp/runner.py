01787dc8c8ab4f09975b98616ddee11f57fd1152
Nils Bade <n.bade@tuhh.de>
2025-01-27 15:13:11 +0100

    Added code to runner to take high resolution screenshots of each page

--- PythonChamberApp/runner.py
+++ PythonChamberApp/runner.py
@@ -7,12 +7,26 @@ import sys
 import process_controller
 import os
 
+
+# def take_screenshot(widget, filename):
+#     # Holen des Screenshots des gesamten Widgets
+#     screenshot = widget.grab()
+#
+#     # Speichern des Screenshots als Bilddatei
+#     screenshot.save(filename, 'png')
+
 if __name__ == "__main__":
     # add extra dll paths before import pyvisa to be able to use keysight pyvisa implementation
     os.add_dll_directory('C:\\Program Files\\Keysight\\IO Libraries Suite\\bin')
     os.add_dll_directory('C:\\Program Files (x86)\\Keysight\\IO Libraries Suite\\bin')
 
     measurement_process_controller = process_controller.ProcessController()
+
+    # """ Save Screenshots for Master-thesis """
+    # for i in range(0,6):
+    #     measurement_process_controller.gui_mainWindow.tabs.setCurrentIndex(i)
+    #     take_screenshot(measurement_process_controller.gui_mainWindow, 'mainWindow'+str(i)+'.png')
+
     sys.exit(measurement_process_controller.gui_app.exec())
 
 
